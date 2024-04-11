a4dfdf5d1ef2c923016b12e33d50bb8ed4e6d990
etorresfdez <100922702+etorresfdez@users.noreply.github.com>
2024-04-11 15:56:06 +0200

    Update asimov_main.cpp

--- sources/asimov_main.cpp
+++ sources/asimov_main.cpp
@@ -264,16 +264,13 @@ void main_asimov::on_parameter_button_clicked()
     }
 }
 
-
 void main_asimov::on_formalism_button_clicked()
 {
     // Variables
     QString texto = ui->textBody->document()->toPlainText();
     QString keyF = "FORMALISM";
-
     QString info_formalism;
     QStringList elementosF;
-
     int num_formalisms = texto.count(keyF);
     QVector<TFormalism> formalisms;
 
@@ -285,50 +282,133 @@ void main_asimov::on_formalism_button_clicked()
     {
         for (int i=1; i<=num_formalisms; i++)
         {
-            // Por cada metanodo encontrado, recogemos sus datos usando section
+            // Por cada formalismo encontrado, recogemos sus datos usando section
             info_formalism = texto.section(keyF, i, i);
 
-            //Separamos por tabulaciones
-            elementosF = info_formalism.split("\t");
+            // Separamos por tabulaciones
+            elementosF = info_formalism.split("\t", Qt::SkipEmptyParts);
 
-            //Eliminamos otros elementos tales como saltos de línea y otros
-            for (QString& elementoF : elementosF) {
-                elementoF = elementoF.simplified();
+            //Eliminamos otros elementos tales como saltos de linea y otros
+            for (QString& elemento : elementosF) {
+                elemento = elemento.simplified();
             }
+            elementosF.removeAll("");
 
-            // Recogida de los datos
+            // Recogida de datos
             QString m_id = elementosF.value(0, "");
             QString m_name = elementosF.value(1, "");
-            QString formula_completa = elementosF.value(2,"");
 
-            //*************  FALLA  ******************
+            QString formula_completa = elementosF.value(2, "");
             QStringList par = formula_completa.split('=');
-            QString m_formalism = par.last().simplified();
+            QString m_formalism = par.back().simplified();
 
-            par.first().remove("[()]");
             QStringList variables = par.first().split(',');
+            variables[0] = variables.first().remove('(');
+            variables[variables.size()-1] = variables.back().remove(')');
             std::vector<QString> m_variables;
             for (QString& var : variables)
             {
                 var = var.simplified();
                 m_variables.push_back(var);
             }
-            //***********************************************
 
             // Creamos el objeto formalismo y lo añadimos a la lista de formalismos
             TFormalism form = TFormalism(m_id, m_name, m_formalism, m_variables);
             formalisms.push_back( form );
         }
 
-        // Una vez recogida la info de todos los metanodos, mostramos los datos por pantalla
+        // Una vez recogida la info de todos los formalismos, mostramos los datos por pantalla
         for (int i=0; i<formalisms.size(); i++)
         {
             ui->selectData->append("FORMALISMO " + QString::number(i+1));
             ui->selectData->append("id: " + formalisms[i].getId());
             ui->selectData->append("nombre: " + formalisms[i].getName());
             ui->selectData->append("formalismo: " + formalisms[i].getFormalism());
-            ui->selectData->append("variables: " + formalisms[i].getVariables() + "\n");
+
+            QString var_mostrar;
+            std::vector<QString> var = formalisms[i].getVariables();
+
+            for (unsigned int i=0; i<var.size(); i++)
+                var_mostrar.append(var[i] + ", ");
+
+            var_mostrar.chop(2);
+
+            ui->selectData->append("variables: " + var_mostrar + "\n");
         }
     }
 }
 
+
+void main_asimov::on_process_button_clicked()
+{
+    // Variables
+    QString texto = ui->textBody->document()->toPlainText();
+    QString keyP = "PROCESS";
+    QString info_process;
+    QStringList elementosP;
+    int num_process = texto.count(keyP);
+    QVector<TProcess> processes;
+
+    if (num_process==0) // No hay procesos en el texto seleccionado
+    {
+        ui->selectData->append("No se encontraron procesos");
+    }
+    else // Sí hay procesos
+    {
+        for (int i=1; i<=num_process; i++)
+        {
+            // Por cada proceso encontrado, recogemos sus datos usando section
+            info_process = texto.section(keyP, i, i);
+
+            // Separamos por tabulaciones
+            elementosP = info_process.split("\t", Qt::SkipEmptyParts);
+
+            //Eliminamos saltos de linea y otros espacios en blanco
+            for (QString& elemento : elementosP) {
+                elemento = elemento.simplified();
+            }
+            elementosP.removeAll("");
+
+            // Recogida de datos
+            QString m_id = elementosP.value(0, "");
+            QString m_description = elementosP.value(1, "");
+            QString m_name = elementosP.value(2, "");
+            QString m_idForm = elementosP.value(3, "");
+            QString substances = elementosP.value(4, "");
+
+            QStringList subs = substances.split(',');
+            subs[0] = subs.first().remove('(');
+            subs[subs.size()-1] = subs.back().remove(')');
+            std::vector<QString> m_substances;
+            for (QString& s : subs)
+            {
+                s = s.simplified();
+                m_substances.push_back(s);
+            }
+
+            // Creamos el objeto proceso y lo añadimos a la lista de procesos
+            TProcess process = TProcess(m_id, m_name, m_description, m_idForm, m_substances);
+            processes.push_back( process );
+        }
+
+        // Una vez recogida la info de todos los procesos, mostramos los datos por pantalla
+        for (int i=0; i<processes.size(); i++)
+        {
+            ui->selectData->append("PROCESO " + QString::number(i+1));
+            ui->selectData->append("id: " + processes[i].getId());
+            ui->selectData->append("descripción: " + processes[i].getDescription());
+            ui->selectData->append("nombre corto: " + processes[i].getName());
+            ui->selectData->append("formalismo: " + processes[i].getIDForm());
+
+            QString subs_mostrar;
+            std::vector<QString> subs = processes[i].getSubstances();
+
+            for (unsigned int i=0; i<subs.size(); i++)
+                subs_mostrar.append(subs[i] + ", ");
+
+            subs_mostrar.chop(2);
+
+            ui->selectData->append("sustancias: " + subs_mostrar + "\n");
+        }
+    }
+}
